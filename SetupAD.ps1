2e368b629a2c0dc88b55d12b001d5a485ca84d2c
LHazy <fhazy2255@gmail.com>
2022-11-19 07:27:38 -0800

    impl install python2 function

--- SetupAD.ps1
+++ SetupAD.ps1
@@ -4,15 +4,27 @@ function Install-AADInternals {
 	Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
 }
 
-function Install-Python {
+function Install-Python3 {
 	Invoke-WebRequest https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe -OutFile ./temp/python-3.11.0-amd64.exe
-	./temp/python-3.11.0-amd64.exe /quiet PrependPath=1 Include_test=0 InstallAllUsers=0 DefaultJustForMeTargetDir=C:\Python311
+	Start-Process ./temp/python-3.11.0-amd64.exe -ArgumentList "/quiet PrependPath=1 Include_test=0 InstallAllUsers=0 DefaultJustForMeTargetDir=C:\Python311"
 	while ($null -eq (Get-Process | ?{$_.ProcessName -match "python-3.11.0-amd64"})) {
 		Start-Sleep -Seconds 5
 		Write-Host "installing python"
 	}
 	write-host "installed python"
 }
+
+function Install-Python2 {
+	#Invoke-WebRequest https://www.python.org/ftp/python/2.7.18/python-2.7.18.amd64.msi -OutFile ./temp/python-2.7.18.amd64.msi
+	#cd temp
+	$p = Start-Process msiexec.exe -ArgumentList "/i python-2.7.18.amd64.msi ALLUSERS=0 TARGETDIR=c:\\python27 /qn" -PassThru
+	while ($null -ne (Get-Process -Id $p.Id -ErrorAction SilentlyContinue)) {
+		Start-Sleep -Seconds 5
+		Write-Host "installing python2"
+	}
+	Write-Host "installed python2"
+}
+
 
 function Install-O365creeper {
 	Invoke-WebRequest https://raw.githubusercontent.com/LMGsec/o365creeper/master/o365creeper.py -OutFile C:\Tools\Azure\o365creeper.py
@@ -27,7 +39,7 @@ try {
 	Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
 	Install-AADInternals
-	Install-Python
+	Install-Python3
 	Install-O365creeper
 } catch {
 	Remove-Item temp
