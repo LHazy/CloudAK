fa0c69198f230ad30d8e78a98dfdf438bed7eb24
LHazy <fhazy2255@gmail.com>
2022-11-19 08:39:16 -0800

    fix python2 install function

--- SetupAD.ps1
+++ SetupAD.ps1
@@ -5,24 +5,25 @@ function Install-AADInternals {
 }
 
 function Install-Python3 {
-	Invoke-WebRequest https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe -OutFile ./temp/python-3.11.0-amd64.exe
-	Start-Process ./temp/python-3.11.0-amd64.exe -ArgumentList "/quiet PrependPath=1 Include_test=0 InstallAllUsers=0 DefaultJustForMeTargetDir=C:\Python311"
-	while ($null -eq (Get-Process | ?{$_.ProcessName -match "python-3.11.0-amd64"})) {
+	cd temp
+	Invoke-WebRequest https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe -OutFile python-3.11.0-amd64.exe
+
+	$p = Start-Process ./python-3.11.0-amd64.exe -ArgumentList "PrependPath=1 Include_test=0 InstallAllUsers=0 DefaultJustForMeTargetDir=C:\Python311"
+	while ($null -ne (Get-Process -Id $p.Id -ErrorAction SilentlyContinue)) {
 		Start-Sleep -Seconds 5
 		Write-Host "installing python"
 	}
+
 	write-host "installed python"
+	cd ../
 }
 
 function Install-Python2 {
-	#Invoke-WebRequest https://www.python.org/ftp/python/2.7.18/python-2.7.18.amd64.msi -OutFile ./temp/python-2.7.18.amd64.msi
-	#cd temp
-	$p = Start-Process msiexec.exe -ArgumentList "/i python-2.7.18.amd64.msi ALLUSERS=0 TARGETDIR=c:\\python27 /qn" -PassThru
-	while ($null -ne (Get-Process -Id $p.Id -ErrorAction SilentlyContinue)) {
-		Start-Sleep -Seconds 5
-		Write-Host "installing python2"
-	}
-	Write-Host "installed python2"
+	cd temp
+	Invoke-WebRequest https://www.python.org/ftp/python/2.7.18/python-2.7.18.amd64.msi -OutFile python-2.7.18.amd64.msi
+	
+	$p = Start-Process msiexec.exe -ArgumentList "/a /ju python-2.7.18.amd64.msi ALLUSERS=0 TARGETDIR=c:\\python27 ADDLOCAL=DefaultFeature,Extensions,Tools,PrependPath,pip_feature" -PassThru
+	Wait-Process -Id $p.Id
 }
 
 
