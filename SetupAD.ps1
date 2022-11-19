eecc77b52ffe2e126da38f3d88e44abca1a7c2f6
LHazy <fhazy2255@gmail.com>
2022-11-19 08:57:43 -0800

    fix install python install function

--- SetupAD.ps1
+++ SetupAD.ps1
@@ -9,12 +9,8 @@ function Install-Python3 {
 	Invoke-WebRequest https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe -OutFile python-3.11.0-amd64.exe
 
 	$p = Start-Process ./python-3.11.0-amd64.exe -ArgumentList "PrependPath=1 Include_test=0 InstallAllUsers=0 DefaultJustForMeTargetDir=C:\Python311"
-	while ($null -ne (Get-Process -Id $p.Id -ErrorAction SilentlyContinue)) {
-		Start-Sleep -Seconds 5
-		Write-Host "installing python"
-	}
+	Wait-Process -Id $p.Id
 
-	write-host "installed python"
 	cd ../
 }
 
@@ -22,8 +18,10 @@ function Install-Python2 {
 	cd temp
 	Invoke-WebRequest https://www.python.org/ftp/python/2.7.18/python-2.7.18.amd64.msi -OutFile python-2.7.18.amd64.msi
 	
-	$p = Start-Process msiexec.exe -ArgumentList "/a /ju python-2.7.18.amd64.msi ALLUSERS=0 TARGETDIR=c:\\python27 ADDLOCAL=DefaultFeature,Extensions,Tools,PrependPath,pip_feature" -PassThru
+	$p = Start-Process msiexec.exe -ArgumentList "/i python-2.7.18.amd64.msi ALLUSERS=0 TARGETDIR=c:\\python27 ADDLOCAL=DefaultFeature,Extensions,Tools,PrependPath,pip_feature" -PassThru
 	Wait-Process -Id $p.Id
+
+	cd ../
 }
 
 
